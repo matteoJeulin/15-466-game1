7b0870eaf4eb9dddf48a7a670a70db09572d6e04
matteoJeulin <matteo.jeulin@gmail.com>
2025-09-01 19:04:24 -0400

    First version of the game is done

--- GameMode.cpp
+++ GameMode.cpp
@@ -16,6 +16,15 @@
 #include <random>
 #include <time.h>
 
+#include <bitset>
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
 // Load all the game sprites into memory
 Sprite const *player_idle = nullptr;
 Sprite const *player_up = nullptr;
@@ -24,8 +33,11 @@ Sprite const *player_left = nullptr;
 Sprite const *player_right = nullptr;
 
 Sprite const *flower = nullptr;
+Sprite const *void_puddle = nullptr;
 
-Sprite const *background_tile = nullptr;
+Sprite const *background_tile1 = nullptr;
+Sprite const *background_tile2 = nullptr;
+Sprite const *background_tile3 = nullptr;
 Sprite const *void_tile = nullptr;
 
 Load<Sprites> sprites(LoadTagDefault, []() -> Sprites const *
@@ -39,8 +51,11 @@ Load<Sprites> sprites(LoadTagDefault, []() -> Sprites const *
 	player_right = &ret.lookup("player_right");
 
 	flower = &ret.lookup("flower");
+	void_puddle = &ret.lookup("void_puddle");
 
-	background_tile = &ret.lookup("background");
+	background_tile1 = &ret.lookup("background1");
+	background_tile2 = &ret.lookup("background2");
+	background_tile3 = &ret.lookup("background3");
 	void_tile = &ret.lookup("void");
 
 	return &ret; });
@@ -74,34 +89,75 @@ GameMode::GameMode()
 	ppu.sprites[PLAYER].index = player_idle->tiles[0].tile_index;
 	ppu.sprites[PLAYER].attributes = player_idle->tiles[0].palette_index;
 
-	srand((unsigned)time(NULL));
-
-	int nb_tiles = 0;
-	int nb_flowers = 3;
-	for (int i = 0; i < nb_flowers; i++)
-	{
-		flowers.push_back(FLOWER + nb_tiles);
-		int flower_x = rand() % 256;
-		int flower_y = rand() % 241;
-		for (Sprite::TileRef tile_ref : flower->tiles)
-		{
-			ppu.sprites[FLOWER + nb_tiles].index = tile_ref.tile_index;
-			ppu.sprites[FLOWER + nb_tiles].attributes = tile_ref.palette_index;
-			ppu.sprites[FLOWER + nb_tiles].x = flower_x + tile_ref.offset_x_chunk * 8;
-			ppu.sprites[FLOWER + nb_tiles].y = flower_y + tile_ref.offset_y_chunk * 8;
-			nb_tiles++;
-		}
-	}
-
-	// Setting the background to the background tile
-	for (unsigned int i = 0; i < ppu.BackgroundWidth * ppu.BackgroundHeight; i++)
-	{
-		ppu.background[i] = flower->tiles[1].tile_index + (flower->tiles[1].palette_index << 8);
-	}
+	setup_map(default_flowers, default_puddles, default_death_time);
 }
 
 GameMode::~GameMode()
 {
+}
+
+void GameMode::setup_map(uint8_t nb_flowers, uint8_t nb_puddles, uint16_t death_time)
+{
+	current_flowers = nb_flowers;
+	current_puddles = nb_puddles;
+	death_timer = death_time;
+
+	srand((unsigned)time(NULL));
+
+	int nb_tiles = FLOWER;
+	for (int i = 0; i < nb_flowers; i++)
+	{
+		flowers.push_back(nb_tiles);
+		int flower_x = rand() % 256;
+		int flower_y = rand() % 241;
+		for (Sprite::TileRef tile_ref : flower->tiles)
+		{
+			ppu.sprites[nb_tiles].index = tile_ref.tile_index;
+			ppu.sprites[nb_tiles].attributes = tile_ref.palette_index;
+			ppu.sprites[nb_tiles].x = flower_x + tile_ref.offset_x_chunk * 8;
+			ppu.sprites[nb_tiles].y = flower_y + tile_ref.offset_y_chunk * 8;
+			nb_tiles++;
+		}
+	}
+
+	for (int i = 0; i < nb_puddles; i++)
+	{
+		void_puddles.push_back(nb_tiles);
+		int puddle_x = rand() % 256;
+		int puddle_y = rand() % 241;
+		for (Sprite::TileRef tile_ref : void_puddle->tiles)
+		{
+			ppu.sprites[nb_tiles].index = tile_ref.tile_index;
+			ppu.sprites[nb_tiles].attributes = tile_ref.palette_index;
+			ppu.sprites[nb_tiles].x = puddle_x + tile_ref.offset_x_chunk * 8;
+			ppu.sprites[nb_tiles].y = puddle_y + tile_ref.offset_y_chunk * 8;
+			nb_tiles++;
+		}
+	}
+
+	// Setting the background to the background tiles
+	Sprite const *background_sprites[3] = {background_tile1, background_tile2, background_tile3};
+	for (unsigned int i = 0; i < ppu.BackgroundWidth * ppu.BackgroundHeight; i++)
+	{
+		int rand_background = rand() % 3;
+		ppu.background[i] = background_sprites[rand_background]->tiles[0].tile_index + (background_sprites[rand_background]->tiles[0].palette_index << 8);
+	}
+}
+
+bool GameMode::colide(uint8_t obj1_x, uint8_t obj1_y, uint8_t obj1_size, uint8_t obj2_x, uint8_t obj2_y, uint8_t obj2_size)
+{
+	// Collision with bottom left corner
+	return (obj1_x >= obj2_x && obj1_x <= obj2_x + obj2_size &&
+			obj1_y >= obj2_y && obj1_y <= obj2_y + obj2_size) ||
+		   // Collision with bottom right corner
+		   (obj1_x + obj1_size >= obj2_x && obj1_x + obj1_size <= obj2_x + obj2_size &&
+			obj1_y >= obj2_y && obj1_y <= obj2_y + obj2_size) ||
+		   // Collision with top left corner
+		   (obj1_x >= obj2_x && obj1_x <= obj2_x + obj2_size &&
+			obj1_y + obj1_size >= obj2_y && obj1_y + obj1_size <= obj2_y + obj2_size) ||
+		   // Collision with top right corner
+		   (obj1_x + obj1_size >= obj2_x && obj1_x + obj1_size <= obj2_x + obj2_size &&
+			obj1_y + obj1_size >= obj2_y && obj1_y + obj1_size <= obj2_y + obj2_size);
 }
 
 bool GameMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
@@ -158,7 +214,8 @@ bool GameMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 }
 
 void GameMode::update(float elapsed)
-{	
+{
+	death_timer--;
 	// Player movement and animation
 	constexpr float PlayerSpeed = 50.0f;
 	if (left.pressed)
@@ -193,26 +250,54 @@ void GameMode::update(float elapsed)
 	}
 
 	// Make sure the player does not leave the screen
-	player_at.x = std::min(player_at.x, float(ppu.ScreenWidth) - player_size.x);
+	player_at.x = std::min(player_at.x, float(ppu.ScreenWidth - player_size));
 	player_at.x = std::max(player_at.x, 0.0f);
 
-	player_at.y = std::min(player_at.y, float(ppu.ScreenHeight) - player_size.y);
+	player_at.y = std::min(player_at.y, float(ppu.ScreenHeight - player_size));
 	player_at.y = std::max(player_at.y, 0.0f);
 
 	// Check collision between flowers and the player
-	for (int flower_index : flowers) {
-		for (size_t i = 0; i < flower->tiles.size(); i++) {
+	for (int flower_index : flowers)
+	{
+		for (size_t i = 0; i < flower->tiles.size(); i++)
+		{
 			// Don't check if the player is colliding with off screen flowers
-			if (ppu.sprites[flower_index + i].y == 240) {
+			if (ppu.sprites[flower_index + i].y == 240)
+			{
 				break;
 			}
 			// If the player collides with a flower tile, move the whole flower offscreen
-			if (player_at.x >= ppu.sprites[flower_index + i].x && player_at.x <= ppu.sprites[flower_index + i].x + 8 &&
-				player_at.y >= ppu.sprites[flower_index + i].y && player_at.y <= ppu.sprites[flower_index + i].y + 8) {
-					for (size_t collided_flower_index = flower_index; collided_flower_index - flower_index < flower->tiles.size(); collided_flower_index++) {
-						ppu.sprites[collided_flower_index].y = 240;
-					}
-					break;
+			if (colide(player_at.x, player_at.y, player_size, ppu.sprites[flower_index + i].x, ppu.sprites[flower_index + i].y, tile_size))
+			{
+				for (size_t collided_flower_index = flower_index; collided_flower_index - flower_index < flower->tiles.size(); collided_flower_index++)
+				{
+					ppu.sprites[collided_flower_index].y = 240;
+				}
+				current_flowers--;
+				break;
+			}
+		}
+	}
+
+	for (int puddle_index : void_puddles)
+	{
+		for (size_t i = 0; i < void_puddle->tiles.size(); i++)
+		{
+			// Don't check if the player is colliding with off screen puddles
+			if (ppu.sprites[puddle_index + i].y == 240)
+			{
+				break;
+			}
+			// If the player collides with a puddle tile, move the whole puddle offscreen and add time to counter
+			if (colide(player_at.x, player_at.y, player_size, ppu.sprites[puddle_index + i].x, ppu.sprites[puddle_index + i].y, tile_size))
+			{
+				for (size_t collided_puddle_index = puddle_index; collided_puddle_index - puddle_index < flower->tiles.size(); collided_puddle_index++)
+				{
+
+					ppu.sprites[collided_puddle_index].y = 240;
+					death_timer += 60 * 2;
+				}
+				break;
 			}
 		}
 	}
@@ -223,6 +308,35 @@ void GameMode::draw(glm::uvec2 const &drawable_size)
 	// Player sprite:
 	ppu.sprites[PLAYER].x = int8_t(player_at.x);
 	ppu.sprites[PLAYER].y = int8_t(player_at.y);
+
+	if (current_flowers == 0)
+	{
+		round++;
+		setup_map(default_flowers + round, std::max(default_puddles - round, 0), default_death_time - 60 * round);
+	}
+	if (death_timer <= 0)
+	{
+		for (unsigned int i = 0; i < ppu.BackgroundWidth * ppu.BackgroundHeight; i++)
+		{
+			ppu.background[i] = void_tile->tiles[0].tile_index + (void_tile->tiles[0].palette_index << 8);
+		}
+		for (int puddle_index : void_puddles)
+		{
+			for (size_t i = 0; i < void_puddle->tiles.size(); i++)
+			{
+				ppu.sprites[puddle_index + i].y = 240;
+				death_timer += 60 * 2;
+			}
+
+			for (int flower_index : flowers)
+			{
+				for (size_t i = 0; i < flower->tiles.size(); i++)
+				{
+					ppu.sprites[flower_index + i].y = 240;
+				}
+			}
+		}
+	}
 
 	ppu.draw(drawable_size);
 }
--- GameMode.hpp
+++ GameMode.hpp
@@ -27,10 +27,38 @@ struct GameMode : Mode {
 	// flower sprites in the tile table
 	std::vector<int> flowers;
 
+	// Number of flowers in game state
+	uint8_t const default_flowers = 5;
+	uint8_t current_flowers = default_flowers;
+
+	// Vector containing all the indexes of the first tile of all
+	// void puddle in the tile table
+	std::vector<int> void_puddles;
+
+	// Number of void puddles in game state
+	uint8_t const default_puddles = 5;
+	uint8_t current_puddles = default_puddles;
+
 	//player position:
 	glm::vec2 player_at = glm::vec2(0.0f);
 
-	glm::vec2 player_size = glm::vec2(8, 8);
+	// Default tile sizes for tiles in the game
+	uint8_t const tile_size = 8;
+	uint8_t const player_size = tile_size;
+
+	// Timer to see when player dies
+	uint16_t const default_death_time = 60 * 10;
+	uint16_t death_timer = default_death_time;
+
+	// Number of rounds played
+	uint16_t round = 0;
+
+	// Function to detect the collision between two objects based on their position and size
+	// Both objects should be squares, it is meant to be used on tiles
+	static bool colide(uint8_t obj1_x, uint8_t obj1_y, uint8_t obj1_size, uint8_t obj2_x, uint8_t obj2_y, uint8_t obj2_size);
+
+	// Sets up the map
+	void setup_map(uint8_t nb_flowers, uint8_t nb_puddles, uint16_t death_time);
 
 	//----- drawing handled by PPU466 -----
 
