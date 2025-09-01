5b320a928925ceb369ca0421505f97e37f914812
matteoJeulin <matteo.jeulin@gmail.com>
2025-09-01 17:13:37 -0400

    Fixed an issue with parsing multi-tile assets

--- parse_ppm.cpp
+++ parse_ppm.cpp
@@ -7,6 +7,15 @@
 #include "data_path.hpp"
 #include "read_write_chunk.hpp"
 
+#include <bitset>
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
 // Define the indexes in a colour
 constexpr int RED = 0;
 constexpr int GREEN = 1;
@@ -23,6 +32,12 @@ void PPM_Parser::parse_chunk(std::string const &filename)
     // Tile this image will be represented as
     PPU466::Tile tile = {0};
 
+    // Jumble the palette so black can be registered.
+    palette[0][RED] = 0xff;
+    palette[1][RED] = 0xff;
+    palette[2][RED] = 0xff;
+    palette[3][RED] = 0xff;
+
     // Set all alpha colours to 0. If alpha is 0
     // the colour hasn't yet been set in this palette
     palette[0][ALPHA] = 0;
@@ -30,11 +45,11 @@ void PPM_Parser::parse_chunk(std::string const &filename)
     palette[2][ALPHA] = 0;
     palette[3][ALPHA] = 0;
 
-    long unsigned int colours_registered = 0;
+    size_t colours_registered = 0;
     bool to_register = true;
+    uint16_t palette_index = palette_table.size();
 
     // Indicates what the index of the current pixel's colour is in the palette
-    long unsigned int colour_index = 0;
 
     int row = chunk_size - 1;
     int column = 0;
@@ -43,118 +58,120 @@ void PPM_Parser::parse_chunk(std::string const &filename)
     int R;
     int G;
     int B;
-    while (file >> R >> G >> B)
     {
-        // Check if the current colour has already been registered in the palette
-        for (long unsigned int i = 0; i <= colours_registered && i < palette.size(); i++)
+        while (file >> R >> G >> B)
         {
-            // If it has, exit the loop
-            if (R == palette[i][RED] && G == palette[i][GREEN] && B == palette[i][BLUE])
+            // Check if the current colour has already been registered in the palette
+            for (size_t i = 0; i <= colours_registered && i < palette.size(); i++)
             {
-                colour_index = i;
-                to_register = false;
-                break;
-            }
-        }
-
-        // If there is still space in the current palette and a new colour
-        // has been found, add it in the palette.
-        if (colours_registered < palette.size() && to_register)
-        {
-            palette[colours_registered][RED] = R;
-            palette[colours_registered][GREEN] = G;
-            palette[colours_registered][BLUE] = B;
-            palette[colours_registered][ALPHA] = 0xff;
-
-            colour_index = colours_registered;
-
-            colours_registered++;
-        }
-        to_register = true;
-    }
-
-    // Search if the palette has already been registered and get its index in the palette table (default is last spot)
-    // Also check if the current palette is contained in or contains another palette (using the alpha channel)
-    uint16_t palette_index = palette_table.size();
-    bool palette_found = false;
-    for (size_t i = 0; i < palette_table.size(); i++)
-    {
-        int nb_colours_found = 0;
-        for (glm::u8vec4 colour : palette)
-        {
-            // If the colour we are checking has an alpha channel that is null,
-            // it means we found all the previous colours of palette in the other
-            // palette and that palette is contained in the other palette.
-            if (colour[ALPHA] == 0)
-            {
-                nb_colours_found = palette.size();
-                break;
-            }
-            bool colour_found = false;
-            for (glm::u8vec4 &registered_colour : palette_table[i])
-            {
-                if (colour[RED] == registered_colour[RED] && colour[GREEN] == registered_colour[GREEN] && colour[BLUE] == registered_colour[BLUE] && colour[ALPHA] == registered_colour[ALPHA])
+                // If it has, exit the loop
+                if (R == palette[i][RED] && G == palette[i][GREEN] && B == palette[i][BLUE])
                 {
-                    colour_found = true;
-                    colour[ALPHA] = 0xff;
+                    to_register = false;
                     break;
                 }
-                // if (registered_colour[ALPHA] == 0) {
-                //     colour[ALPHA] = 0xff;
-                //     registered_colour = colour;
-                //     colour_found = true;
-                //     break;
-                // }
             }
-            if (!colour_found)
+
+            // If there is still space in the current palette and a new colour
+            // has been found, add it in the palette.
+            if (colours_registered < palette.size() && to_register)
             {
+                palette[colours_registered][RED] = R;
+                palette[colours_registered][GREEN] = G;
+                palette[colours_registered][BLUE] = B;
+                palette[colours_registered][ALPHA] = 0xff;
+
+                colours_registered++;
+            }
+            to_register = true;
+        }
+
+        // Search if the palette has already been registered and get its index in the palette table (default is last spot)
+        // Also check if the current palette is contained in or contains another palette (using the alpha channel)
+        bool palette_found = false;
+        for (size_t i = 0; i < palette_table.size(); i++)
+        {
+            int nb_colours_found = 0;
+            for (glm::u8vec4 colour : palette)
+            {
+                // If the colour we are checking has an alpha channel that is null,
+                // it means we found all the previous colours of palette in the other
+                // palette and that palette is contained in the other palette.
+                if (colour[ALPHA] == 0)
+                {
+                    nb_colours_found = palette.size();
+                    palette = palette_table[i];
+                    break;
+                }
+                bool colour_found = false;
+                for (glm::u8vec4 &registered_colour : palette_table[i])
+                {
+                    if (colour[RED] == registered_colour[RED] && colour[GREEN] == registered_colour[GREEN] && colour[BLUE] == registered_colour[BLUE] && colour[ALPHA] == registered_colour[ALPHA])
+                    {
+                        colour_found = true;
+                        colour[ALPHA] = 0xff;
+                        break;
+                    }
+                    // if (registered_colour[ALPHA] == 0) {
+                    //     colour[ALPHA] = 0xff;
+                    //     registered_colour = colour;
+                    //     colour_found = true;
+                    //     break;
+                    // }
+                }
+                if (!colour_found)
+                {
+                    break;
+                }
+                else
+                {
+                    nb_colours_found++;
+                    colour_found = false;
+                }
+            }
+            if (nb_colours_found == palette.size())
+            {
+                palette_index = i;
+                palette_found = true;
                 break;
             }
             else
             {
-                nb_colours_found++;
-                colour_found = false;
+                nb_colours_found = 0;
             }
         }
-        if (nb_colours_found == palette.size())
+        if (!palette_found)
         {
-            palette_index = i;
-            palette_found = true;
-            break;
+            palette_table.push_back(palette);
+            palette_index = palette_table.size() - 1;
         }
-        else
-        {
-            nb_colours_found = 0;
-        }
-    }
-    if (!palette_found)
-    {
-        palette_table.push_back(palette);
-        palette_index = palette_table.size() - 1;
     }
 
     file.close();
     file.open(filename);
     // Make sure to only parse the pixels in the image
-    while (file >> R >> G >> B && pixel_count < chunk_size * chunk_size)
     {
-        // Find the current colour in the palette
-        for (long unsigned int i = 0; i <= colours_registered && i < palette.size(); i++)
+        size_t colour_index = 0;
+        while (file >> R >> G >> B && pixel_count < chunk_size * chunk_size)
         {
-            if (R == palette[i][RED] && G == palette[i][GREEN] && B == palette[i][BLUE])
+            // Find the current colour in the palette
+            for (size_t i = 0; i <= colours_registered && i < palette.size(); i++)
             {
-                colour_index = i;
-                break;
+                if (R == palette[i][RED] && G == palette[i][GREEN] && B == palette[i][BLUE])
+                {
+                    colour_index = i;
+                    break;
+                }
             }
-        }
 
-        // Add the pixel to the tile
-        tile.bit0[row] += (colour_index % 2) << column;
-        tile.bit1[row] += ((colour_index >> 1) % 2) << column;
-        pixel_count++;
-        column = (column + 1);
-        row -= column / chunk_size;
-        column %= chunk_size;
+            // Add the pixel to the tile
+            tile.bit0[row] += (colour_index % 2) << column;
+            tile.bit1[row] += ((colour_index >> 1) % 2) << column;
+            pixel_count++;
+            column = (column + 1);
+            row -= column / chunk_size;
+            column %= chunk_size;
+        }
     }
 
     // Constructing the tile ref for our new tile
@@ -217,11 +234,11 @@ void PPM_Parser::parse_image(std::string const &filename, std::string const &out
                 parse_chunk(output_file);
 
                 tile_refs.back().offset_x_chunk = chunks_parsed_row;
-    
+
                 // The file is read from top left to bottom right
                 // but the sprite is displayed from bottom left to top right
                 tile_refs.back().offset_y_chunk = chunks_in_column - 1 - chunks_parsed_column;
-                
+
                 chunks_parsed_row += 1;
 
                 // Empty all the text from the output file to make space for the new chunk.
@@ -243,7 +260,7 @@ void PPM_Parser::parse_image(std::string const &filename, std::string const &out
                     in_file >> width >> height >> colours;
 
                     // Number of pixels until new line in chunk
-                    uint8_t pixels_to_chunk = width * chunks_parsed_column + chunk_size * chunks_parsed_row;
+                    uint8_t pixels_to_chunk = width * chunks_parsed_column * chunk_size + chunk_size * chunks_parsed_row;
 
                     // Navigate to new chunk line
                     while (pixels_to_chunk > 0 && in_file >> R >> G >> B)
